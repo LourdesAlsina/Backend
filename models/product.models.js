18924f814a6ac9575de073b6ca268c4af57114c5
LourdesAlsina <lulialsinaa@gmail.com>
2023-07-09 19:32:35 -0300

    correccion

--- models/product.models.js
+++ models/product.models.js
@@ -4,12 +4,13 @@ const productCollection = 'product';
 
 const productSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    stock: { type: Number, required: true },
-    code: { type: String, required: true },
-    category: { type: String, required: true },
-    status: { type: Boolean, default: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  thumbnails: { type: [String], default: [] },
+  code: { type: String, required: true, unique: true },
+  category: { type: String, required: true },
+  stock: { type: Number, required: true },
+  status: { type: Boolean, default: true },
 });
 
 mongoose.set("strictQuery", false);
