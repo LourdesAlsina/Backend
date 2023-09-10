029528b6c157a6a02103ccff9d7650bbb5a5354c
LourdesAlsina <lulialsinaa@gmail.com>
2023-09-10 18:36:54 -0300

    desafio clase 32

--- services/index.js
+++ services/index.js
@@ -2,7 +2,10 @@ import ProductDAO from "../Dao/product.mongo.dao";
 import CartDAO from "../Dao/product.mongo.dao";
 import ProductRepository from "../repositories/product.repository";
 import CartRepository from "../repositories/cart.repository";
+import UserDAO from "../Dao/user.mongo.dao";
+import UserRepository from "../repositories/user.repository";
 
 
 export const ProductService = new ProductRepository(new ProductDAO())
 export const CartService = new CartRepository(new CartDAO())
+export const UserService = new UserRepository(new UserDAO())
